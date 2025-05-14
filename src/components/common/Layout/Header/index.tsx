39bd077599f1bb90b1d7132d0a4cd24f51bb7525
nerte <grimza@nerteui-MacBookPro.local>
2025-05-15 02:21:27 +0900

    Style : Header UI, 반응형

---
+++ src/components/common/Layout/Header/index.tsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+import InputField from '../../InputField';
+
+export function Header() {
+  //받아오는 데이터에 따라 텍스트가 내가게 or 내 프로필
+
+  return (
+    <div className="fixed top-0 left-0 flex w-full flex-col items-center justify-center gap-4 bg-white px-5 py-2.5 md:px-8 md:py-[15px]">
+      <div className="flex h-full w-full max-w-255 items-center justify-between">
+        <div className="flex items-center gap-8.75 lg:gap-10">
+          <img
+            src="src/public/images/logo.png"
+            alt="thejulge 로고"
+            className="h-3.75 w-20.5 lg:h-5 lg:w-27.25"
+          />
+          <div className="hidden h-10 w-86 md:block">
+            <InputField inputType="input" onChange={() => {}} />
+          </div>
+        </div>
+        <div className="text-lg-bold flex items-center gap-4 md:gap-3 lg:gap-10">
+          <Link to={'/'}>텍스트</Link>
+          <p>로그아웃</p>
+          <div>알림 드롭다운</div>
+        </div>
+      </div>
+      <div className="block h-10 w-full md:hidden">
+        <InputField inputType="input" onChange={() => {}} />
+      </div>
+    </div>
+  );
+}
--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from './Header';
 
 export function Layout() {
   return (
     <div className="flex h-dvh w-dvw items-center justify-center">
       <div className="w-full px-3 py-10 md:px-8 md:py-15 lg:max-w-[965px]">
+        <Header />
         <Outlet />
       </div>
     </div>
