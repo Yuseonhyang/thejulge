29de4b42f04a94611d929e8e5dbb7323d42637e0
nerte <grimza@nerteui-MacBookPro.local>
2025-05-15 02:45:06 +0900

    Feat : create SearchInput

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,31 +1,17 @@
-import NotificationDropdown from '../../components/common/Dropdown/NotificationDropdown';
-import SelectDropdown from '../../components/common/Dropdown/SelectDropdown';
-import useModalContext from '../../components/common/Modal/core/useModalContext';
-import Modal from '../../components/common/Modal';
-import ModalPortal from '../../components/common/Modal/core/ModalPortal';
+// import NotificationDropdown from '../../components/common/Dropdown/NotificationDropdown';
+// import SelectDropdown from '../../components/common/Dropdown/SelectDropdown';
+// import useModalContext from '../../components/common/Modal/core/useModalContext';
+// import Modal from '../../components/common/Modal';
+// import ModalPortal from '../../components/common/Modal/core/ModalPortal';
+import InputField from '../../components/common/InputField';
 
 export default function Home() {
-  const options = ['테스트', '테스트2', '테스트3'];
-  const { openModal } = useModalContext();
+  // const options = ['테스트', '테스트2', '테스트3'];
+  // const { openModal } = useModalContext();
 
   return (
     <div>
-      <SelectDropdown
-        containerWidth="300"
-        options={options}
-        onSelect={() => {}}
-        triggerWidth="500"
-      />
-      <NotificationDropdown notifications={[]} hasNewNotification={true} onMarkAsRead={() => {}} />
-
-      <div>
-        <div onClick={() => openModal('test')}>id로 열림 버튼</div>
-        <ModalPortal modalId="test">
-          <Modal modalId="test">
-            <div>컨텐츠</div>
-          </Modal>
-        </ModalPortal>
-      </div>
+      <InputField inputType="search" onChange={() => {}} leftSlot={<div>슬롯</div>} />
     </div>
   );
 }
