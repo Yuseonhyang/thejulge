194ad1c560d4570da165ffdfebaad838af9f5a05
nerte <grimza@nerteui-MacBookPro.local>
2025-05-18 03:37:51 +0900

    Feat : 성공후 user type 에따라 분기 처리

--- src/components/AuthForm/hooks/useAuth.ts
+++ src/components/AuthForm/hooks/useAuth.ts
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import { LoginFormData } from '../types/auth';
 import { INITIAL_LOGIN_VALUE } from '../initial-value';
 import axiosClient from '../../../lib/instance';
+import { useNavigate } from 'react-router-dom';
 
 export default function useAuth() {
   const [formData, setFormData] = useState<LoginFormData>(INITIAL_LOGIN_VALUE);
+
+  const navigate = useNavigate();
 
   const handleChangeAuthForm = (
     key: string,
@@ -16,7 +19,16 @@ export default function useAuth() {
   };
 
   const loginUser = async () => {
-    const { data } = await axiosClient.post(`/token`, { ...formData });
+    try {
+      const { data } = await axiosClient.post(`/token`, { ...formData });
+      localStorage.setItem('accessToken', data.item.token);
+
+      if (data.item.user.item.type === 'employee') return navigate(`/mypage`);
+      if (data.item.user.item.type === 'employer') return navigate(`/mystore`);
+    } catch (err) {
+      console.log(err);
+      //todo에러 핸들링
+    }
   };
   return { handleChangeAuthForm, loginUser };
 }
