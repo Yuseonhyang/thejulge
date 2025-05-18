1d1a3d9636869890eca8c246f4ba30955e78180a
nerte <grimza@nerteui-MacBookPro.local>
2025-05-19 04:23:11 +0900

    Style : create NoProfile.tsx

--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -1,7 +1,12 @@
 export default function MyPage() {
   return (
     <div>
-      <div>페이지</div>
+      <section>
+        <h1 className="text-3xl-bold">내 프로필</h1>
+      </section>
+      <section>
+        <h1 className="text-3xl-bold">신청내역</h1>
+      </section>
     </div>
   );
 }
