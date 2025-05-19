fc5058c80c13d4148a7bd1915c675cb722005a32
nerte <grimza@nerteui-MacBookPro.local>
2025-05-20 01:22:04 +0900

    Style : mypage-styling

--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -3,12 +3,12 @@ import Table from './_myPage/components/Table';
 
 export default function MyPage() {
   return (
-    <div>
-      <section>
+    <div className="flex flex-col gap-20">
+      <section className="flex flex-col gap-4 md:gap-8">
         <h1 className="text-3xl-bold">내 프로필</h1>
         <MyProfile />
       </section>
-      <section>
+      <section className="flex flex-col gap-4 md:gap-8">
         <h1 className="text-3xl-bold">신청내역</h1>
         <Table />
       </section>
