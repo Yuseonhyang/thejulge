e80153ecab1a6a2fb5a74254a9f59975294c49a4
nerte <grimza@nerteui-MacBookPro.local>
2025-05-20 02:31:08 +0900

    Feat : getUserInfo() 함수 생성 (탠스택쿼리)

--- src/pages/myStore/index.tsx
+++ src/pages/myStore/index.tsx
@@ -1,19 +1,23 @@
 import MyRecruits from './_myStorePage/components/MyRecruits';
 import MyStore from './_myStorePage/components/MyStore';
 import { INITIAL_STORE } from './_myStorePage/value/initiail-value';
+import { getUserInfo } from '../../hooks/use-Tanstack-query';
+import NoStore from './_myStorePage/components/NoStore';
+import NoRecruit from './_myStorePage/components/NoRecruit';
 
 export default function MyStorePage() {
-  //내가게 정보 조회 후에 noStore , noRecruit처리
+  const data = getUserInfo();
+  const shop = data?.item.shop;
 
   return (
     <div className="flex flex-col gap-20">
       <section className="flex flex-col gap-4 md:gap-8">
         <h1 className="text-3xl-bold">내 가게</h1>
-        <MyStore store={INITIAL_STORE} />
+        {!shop ? <NoStore /> : <MyStore store={INITIAL_STORE} />}
       </section>
       <section className="flex flex-col gap-4 md:gap-8">
         <h1 className="text-3xl-bold">내 공고</h1>
-        <MyRecruits />
+        {!shop ? <NoRecruit /> : <MyRecruits />}
       </section>
     </div>
   );
