e502a2a4e9d021ce5b15f88aee1b892f43e52606
nerte <grimza@nerteui-MacBookPro.local>
2025-05-20 04:26:42 +0900

    Style : layout update

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -3,7 +3,7 @@ import { Header } from './Header';
 
 export function Layout() {
   return (
-    <div className="h-dvh w-dvw items-center justify-center">
+    <div className="flex h-dvh w-dvw justify-center">
       <Header />
       <div className="mt-[102px] w-full px-3 py-10 md:mt-[70px] md:px-8 md:py-15 lg:max-w-[965px]">
         <Outlet />
