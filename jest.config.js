32900a2b848eba34854b5a26606d893cdcbfc3b5
nerte <grimza@nerteui-MacBookPro.local>
2025-06-11 02:18:11 +0900

    Chore : 테스트 에러 해결

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
-// jest.config.ts
 export default {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
