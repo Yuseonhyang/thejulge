15dcdd272e94a27edbfc2af242aba81ebbcd8868
nerte <grimza@nerteui-MacBookPro.local>
2025-06-11 15:15:14 +0900

    Fix : 테스트 에러 해결을 위한 env import 문 해결

--- src/lib/instance.ts
+++ src/lib/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { PATHS } from '../constants/path';
 
-const BASE_URL = import.meta.env.VITE_PUBLIC_API_URL;
+const BASE_URL = process.env.VITE_PUBLIC_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
