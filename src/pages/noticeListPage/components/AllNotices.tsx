4274df2414bb5941287ea2d61597861722c2a154
nerte <grimza@nerteui-MacBookPro.local>
2025-06-11 14:36:10 +0900

    Feat : allNotices 컴포넌트의 NotecesCard 데이터 패칭

--- src/pages/noticeListPage/components/AllNotices.tsx
+++ src/pages/noticeListPage/components/AllNotices.tsx
@@ -1,9 +1,30 @@
 import Pagination from '../../../components/Table/Pagination';
+import NoticesCard from '../../myStore/components/NoticesCard';
+import { Notices } from '../../myStore/types/notice';
 
-export default function AllNotices() {
+interface Props {
+  notices: Notices;
+}
+export default function AllNotices({ notices }: Props) {
+  const { items } = notices;
   return (
     <div className="flex flex-col gap-[30px] md:gap-10">
-      <div className="grid grid-cols-2 gap-1 md:gap-4 lg:grid-cols-3">전체 공고카드 </div>
+      <div className="grid grid-cols-2 gap-1 md:gap-4 lg:grid-cols-3">
+        {items.length > 0 &&
+          items.map((item) => {
+            const { name, address1, imageUrl } = item.item.shop;
+
+            return (
+              <NoticesCard
+                key={item.item.id}
+                notice={item.item}
+                name={name}
+                address1={address1}
+                image={imageUrl}
+              />
+            );
+          })}
+      </div>
       <Pagination />
     </div>
   );
--- src/pages/noticeListPage/index.tsx
+++ src/pages/noticeListPage/index.tsx
@@ -3,9 +3,10 @@ import AllNotices from './components/AllNotices';
 import NoticesFilters from './components/NoticesFilters';
 import RecommendedNotices from './components/RecommendedNotices';
 import { getNoticeList } from '../../api/notices';
+import { Notices } from '../myStore/types/notice';
 
 export default function NoticeListPage() {
-  const [allNotices, setAllNotices] = useState();
+  const [allNotices, setAllNotices] = useState<Notices>();
   const fetchAllNotices = async () => {
     const data = await getNoticeList();
     setAllNotices(data);
@@ -15,6 +16,7 @@ export default function NoticeListPage() {
     fetchAllNotices();
   }, []);
 
+  if (!allNotices) return;
   return (
     <div className="flex w-full flex-col gap-20">
       <section className="flex flex-col gap-4 md:gap-8">
@@ -26,7 +28,7 @@ export default function NoticeListPage() {
           <h1 className="text-3xl-bold">전체 공고</h1>
           <NoticesFilters />
         </div>
-        <AllNotices />
+        <AllNotices notices={allNotices} />
       </section>
     </div>
   );
