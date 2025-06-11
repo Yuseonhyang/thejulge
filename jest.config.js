a2dd5cd876d8c445ed9d732cf11d555981063ead
nerte <grimza@nerteui-MacBookPro.local>
2025-06-11 15:57:15 +0900

    Fix : test 파일 오류 해결

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 export default {
   preset: 'ts-jest/presets/js-with-ts',
 
+  setupFiles: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup-env.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.json' }],
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,6 @@
 // jest.setup.ts
-import '@testing-library/jest-dom';
+import { TextEncoder } from 'util';
+
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder;
+}
