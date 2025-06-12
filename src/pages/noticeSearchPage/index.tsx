c0e36eea34dc6e93c4029b4fca1008414e527a37
nerte <grimza@nerteui-MacBookPro.local>
2025-06-12 15:25:00 +0900

    Feat : 검색 페이지에 필터 기능

--- src/pages/noticeSearchPage/index.tsx
+++ src/pages/noticeSearchPage/index.tsx
@@ -5,6 +5,7 @@ import { Notices } from '../myStore/types/notice';
 import { getNoticeList } from '../../api/notices';
 import { INITIAL_FILTER } from '../../components/common/Dropdown/FilterDropdown/value/initial-value';
 import NoticesFilters from '../../components/NoticesFilters';
+import { SelectedFilterType } from '../../components/common/Dropdown/FilterDropdown/types';
 
 export default function NoticeSearchPage() {
   const [searchParams] = useSearchParams();
@@ -19,12 +20,15 @@ export default function NoticeSearchPage() {
   const fetchNotices = async () => {
     const data = await getNoticeList(noticeListFilter, keyword);
     setSearchNotices(data);
-    console.log(data);
   };
 
   useEffect(() => {
     fetchNotices();
-  }, []);
+  }, [noticeListFilter, keyword]);
+
+  const changeNoticeListFilter = (key: string, value: string | SelectedFilterType) => {
+    setNoticeListFilter((prev) => ({ ...prev, [key]: value }));
+  };
 
   if (!searchNotices) return;
 
@@ -34,7 +38,14 @@ export default function NoticeSearchPage() {
         <h1 className="text-3xl-bold">
           <span className="text-primary">{keyword}</span>에 대한 공고 목록
         </h1>
-        <NoticesFilters onSelectFilter={() => {}} onSelectSort={() => {}} />
+        <NoticesFilters
+          onSelectSort={(sortOption: string) => {
+            changeNoticeListFilter('sortOption', sortOption);
+          }}
+          onSelectFilter={(filters: SelectedFilterType) => {
+            changeNoticeListFilter('filters', filters);
+          }}
+        />
       </div>
       <section className="flex flex-col gap-4 md:gap-8">
         <div className="grid grid-cols-2 lg:grid-cols-3">{}</div>
