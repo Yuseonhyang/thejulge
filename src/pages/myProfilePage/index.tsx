418da1e6054e6330154800524de365dc0e2cb22b
grimza99 <gbtmxlf@naver.com>
2025-06-13 18:27:46 +0900

    Feat : myProfile 페이지에서 초기 데이터 패칭

--- src/pages/myProfilePage/index.tsx
+++ src/pages/myProfilePage/index.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 import Table from '../../components/Table';
+import axiosInstance from '../../lib/instance';
+import decodeJWT from '../../utils/decode-jwt';
 import ProfileCard from './components/ProfileCard';
+import { ProfileType } from './types/profile';
+import NoProfile from './components/NoProfile';
 
 export default function MyProfilePage() {
+  const [profile, setProfile] = useState<ProfileType>();
+  const { userId } = decodeJWT();
+
+  const fetchProfile = async () => {
+    if (!userId) return;
+    const { data } = await axiosInstance(`users/${userId}`);
+    setProfile(data.item);
+  };
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
   return (
     <div className="flex flex-col gap-20">
       <section className="flex flex-col gap-4 md:gap-8">
         <h1 className="text-3xl-bold">내 프로필</h1>
-        <ProfileCard />
+        {profile?.name ? <ProfileCard profile={profile} /> : <NoProfile />}
       </section>
       <section className="flex flex-col gap-4 md:gap-8">
         <h1 className="text-3xl-bold">신청내역</h1>
