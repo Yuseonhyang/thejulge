ab97bfc310d42359349a5648e0b927c6c90943cd
grimza99 <gbtmxlf@naver.com>
2025-06-13 18:40:48 +0900

    Chore : 공고 보러가기 path 정하고 상수화

--- src/pages/myProfilePage/components/NoApply.tsx
+++ src/pages/myProfilePage/components/NoApply.tsx
@@ -1,8 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '../../../components/common/Button';
 import { MYPAGE_BUTTON } from '../../../constants/button';
-
-//todo : 공고 보러가기 path 정하고 상수화
+import { PATHS } from '../../../constants/path';
 
 export default function NoApply() {
   const navigate = useNavigate();
@@ -11,7 +10,7 @@ export default function NoApply() {
     <div className="border-gray20 flex w-full flex-col items-center justify-center gap-6 rounded-xl border-1 py-15">
       <p className="text-md-rg md:text-lg-rg">아직 신청 내역이 없어요.</p>
       <div className="h-12 w-27 md:w-86.5">
-        <Button variant="primary" size="parent-dependent" onClick={() => navigate('/')}>
+        <Button variant="primary" size="parent-dependent" onClick={() => navigate(PATHS.NOTICES)}>
           {MYPAGE_BUTTON.noApply}
         </Button>
       </div>
